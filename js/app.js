0db21d69cfb771fecb2b75d14e46d91551b55b3e
Vinícius Victorino <viniciusflv@gmail.com>
2018-04-08 16:53:44 -0300

    Basic Functionality

--- js/app.js
+++ js/app.js
@@ -1,46 +1,95 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(position) {
+    this.row = 83
+    this.col = 101
+    
+    this.x = this.col * -1 * Math.random()
+    this.y = this.row * position
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.speed = Math.random() * (3 - .5) + .5;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    if(player.y !== 0) {
+        this.x += this.col * this.speed * dt
+        if(this.x > 505) {
+            this.x = this.col * -1 * Math.random()
+            this.speed = Math.random() * (3 - .5) + .5
+        }
+    } else {
+        this.y += this.row/5
+        if(this.y > 1000) {
+            player.x = this.col * 2
+            player.y = this.row * 5
+            player.allowedKeys = null
+            setInterval(() => {
+                if(player.y === this.row * 5) {
+                    alert('Congratulations! YOU WON!')
+                    location.reload()
+                }                
+            }, 1000)
+        }
+    }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function(char) {
+    this.row = 83
+    this.col = 101
 
+    this.x = this.col * 2
+    this.y = this.row * 5
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    this.sprite = `images/char-${char}.png`
+    this.allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
+}
 
-    player.handleInput(allowedKeys[e.keyCode]);
+Player.prototype.update = function() {
+    for(enemy of allEnemies) {
+        if((enemy.x +50 > this.x && enemy.x < this.x +50) && (enemy.y +50 > this.y && enemy.y < this.y +50)) {
+            alert('Try Again')
+            location.reload()
+        }
+    }
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'up':
+            this.y === 0 ? this.y : this.y -= this.row
+        break;
+    
+        case 'down':
+            this.y === 415 ? this.y : this.y += this.row
+        break;
+        
+        case 'left':
+            this.x === 0 ? this.x : this.x -= this.col
+        break;
+        
+        case 'right':
+            this.x === 404 ? this.x : this.x += this.col
+        break;
+        
+    }
+}
+
+var allEnemies = [new Enemy(1), new Enemy(2), new Enemy(3)]
+var player = new Player('boy')
+
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = player.allowedKeys
+    if(allowedKeys) player.handleInput(allowedKeys[e.keyCode]);
 });
